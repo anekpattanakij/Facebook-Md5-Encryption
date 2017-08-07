528cdae33df13dcc340d9fb8166286a131baab64
anekpattanakij <a.anekpattanakij@gmail.com>
2017-08-07 15:45:48 +0700

    Update command arguments

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ var fs = require('fs');
 
 var NEW_LINE = '\r\n';
 var FILE_ENCODING = 'utf8'; 
+var DEFAULT_INPUT_FILENAME = 'input.csv';
+var DEFAULT_OUTPUT_FILENAME = 'encryptFile.csv'
 
+var inputFilename = DEFAULT_INPUT_FILENAME;
+var outFilename = DEFAULT_OUTPUT_FILENAME;
+var includeHeader = false;
 
-// Make sure we got a filename on the command line.
-/* if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
-}*/
+if (process.argv.length > 2) {
+  inputFilename = process.argv[2];
+}
+if (process.argv.length > 3) {
+  outFilename = process.argv[3];
+}
+if (process.argv.length > 4) {
+  includeHeader = process.argv[4].toUpperCase() === 'Y';
+}
 
-var logger = fs.createWriteStream('encryptFile.csv', {
+var logger = fs.createWriteStream(outFilename, {
   flags: 'w' 
 });
 
@@ -19,16 +28,20 @@ function addNewMessageWithNewLine(logger, message) {
   logger.write(message + NEW_LINE);
 }
 
-fs.readFile('./input.csv', FILE_ENCODING, function read(err, data) {
+fs.readFile('./' +inputFilename, FILE_ENCODING, function read(err, data) {
     if (err) {
         throw err;
     }
     content = data;
-    console.log(data);
     var content = data.split("\r\n");
     for(var i=0;i<content.length;i++) {
+      if(includeHeader && i===0)
+      {
+        addNewMessageWithNewLine(logger,content[i]);
+        continue;
+      }
       addNewMessageWithNewLine(logger,sha256(content[i]));
     }
-    console.log(content);
+    console.log('Complete !!');
 });
 
